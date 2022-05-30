cb901a7b92b845a9cf38c56af888bfbde3ad84d7
Bao <bao.lg1998@gmail.com>
2022-05-30 10:07:43 +0700

    Initial Commit

---
+++ EnglishFlashing/EnglishFlashingApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnglishFlashingApp.swift
+//  EnglishFlashing
+//
+//  Created by bao le gia on 30/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct EnglishFlashingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EnglishFlashingUITests/EnglishFlashingUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  EnglishFlashingUITestsLaunchTests.swift
+//  EnglishFlashingUITests
+//
+//  Created by bao le gia on 30/05/2022.
+//
+
+import XCTest
+
+class EnglishFlashingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
